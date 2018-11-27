b81e2c9c7db2e2e08dcf249b45b134b5d8f30447
Koolo <43411405+koolo233@users.noreply.github.com>
2018-11-27 08:37:13 +0800

    Update load_data_use_csv.py

--- chest X-ray images classify/load_data_use_csv.py
+++ chest X-ray images classify/load_data_use_csv.py
@@ -1,9 +1,32 @@
 """
-此文件用来从csv文件中读取标签并从相关图片目录中读取图片
+此文件用来从csv文件中读取标签并从相关图片目录中读取图片,
+允许设定最大读取量，如果设定最大读取量则需要传入总类数，以保证每一个类别都有近似相同数量的样本
 
 相关函数有两个
 read_image():从对应目录中按照相关要求读取图片并以np.array的形式返回图片数据
 load_data_use_csv():从csv文件中读取标签、从read_image()请求图片数据并返回对应图片和标签
+
+This file is used to read labels from CSV files and pictures from related picture directories
+Maximum reads are allowed, and if maximum reads are set, the total number of classes will need to be passed in to
+ensure that each class has approximately the same number of samples.
+
+There are two correlation functions.
+Read_image(): Read the picture from the corresponding directory according to the relevant requirements and
+              return the picture data in the form of np.array
+
+Load_data_use_csv(): Read labels from CSV files, request image data from read_image() and
+                     return corresponding pictures and labels
+
+
+
+History:
+
+++++++++++++++++++++++++++++create+++++++++++++++++++++++++
+Author: Koolo233 <Koolo233@163.com>
+Created: 2018-11-24
+++++++++++++++++++++++++++++update+++++++++++++++++++++++++
+Author:
+
 """
 
 import numpy as np
@@ -16,10 +39,17 @@ def read_image(img_name, IM_WIDTH, IM_HEIGHT, flag_colorful=True):
 
     """
     :param img_name: 图片名
+                     image name
     :param IM_WIDTH: 图片的宽度(像素)
+                      the width of image
     :param IM_HEIGHT: 图片的高度(像素)
+                      the height of image
     :param flag_colorful: 是否为3通道图片，True为是，False为单通道， 默认为True
+                           whether 3 channels
+                           True: 3 channels
+                           False: 1 channels
     :return: 以np.array的形式返回图片数据
+              return the data of image in the form of np.array
     """
 
     if flag_colorful:
@@ -38,15 +68,31 @@ def load_data_use_csv(image_path, label_path, csv_col_name, csv_col_value, IM_WI
 
     """
     :param image_path: 图片所在文件夹路径
+                        the path of images
     :param label_path: csv文件所在路径
+                        the path of labels
     :param csv_col_name: csv文件 图片名列名
+                        the column name of images in CSV file
     :param csv_col_value: csv文件 标签名列名
+                        the column name of labels in CSV file
     :param IM_WIDTH: 图片设定宽度(像素)
+                        resize images' width
     :param IM_HEIGHT: 图片设定高度(像素)
+                        resize images' height
     :param max_load: 最大图片加载量，默认为加载全部
+                        the max number of loading
+                        default:all
     :param flag_colorful: 是否为3通道，True为是3通道，False为1通道，默认为True
+                        whether 3 channels
+                        True: 3 channels
+                        else: 1 channels
+                        default: True
     :param n_class: 总分类数，如果指定最大图片加载量，则需要传入此值，每一种类别的图片返回的数量为 int(max_load/n_class)
+                    the number of classes
+                    if setting the max number of loading, this param is necessary
+                    the number of each labels will be int(max_load/n_class)
     :return: 以np.array的形式返回数据和标签 数据在前、标签在后
+            return the data in the form of np.array, (images. labels)
     """
 
     print('Begin to load data')
@@ -61,6 +107,7 @@ def load_data_use_csv(image_path, label_path, csv_col_name, csv_col_value, IM_WI
     image_load_every_label = []
     img_load_num = 0
 
+    # 是否设定最大读入量
     if max_load and n_class:
         for i in range(n_class):
             image_load_every_label.append(0)
@@ -75,7 +122,7 @@ def load_data_use_csv(image_path, label_path, csv_col_name, csv_col_value, IM_WI
             if max_load and n_class:
                 if image_load_every_label[int(result_dist[fn[:-4]])] <= max_load_everylabel:
 
-                    fd = os.path.join('./train_dir', fn)
+                    fd = os.path.join(image_path, fn)
                     images.append(read_image(fd, IM_WIDTH, IM_HEIGHT, flag_colorful))
                     label.append(result_dist[fn[:-4]])
                     img_load_num += 1
@@ -84,7 +131,7 @@ def load_data_use_csv(image_path, label_path, csv_col_name, csv_col_value, IM_WI
                     if img_load_num % 1000 == 0:
                         print(img_load_num)
             else:
-                fd = os.path.join('./train_dir_raw', fn)
+                fd = os.path.join(image_path, fn)
                 images.append(read_image(fd, IM_WIDTH, IM_HEIGHT, flag_colorful))
                 label.append(result_dist[fn[:-4]])
                 img_load_num += 1
